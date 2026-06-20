df34098963616324239f2b8530dfe4391a7cb07b
Alejandro Marin <3019371840101@ingenieria.usac.edu.gt>
2026-06-20 01:33:28 -0600

    deploy

--- Practica3/smartinvoice/backend/app/services/ocr_service.py
+++ Practica3/smartinvoice/backend/app/services/ocr_service.py
@@ -1,17 +1,27 @@
 import easyocr
 
+reader = None
 
-reader = easyocr.Reader(
-    ['es'],
-    gpu=False
-)
+
+def get_reader():
+
+    global reader
+
+    if reader is None:
+
+        reader = easyocr.Reader(
+            ['es'],
+            gpu=False
+        )
+
+    return reader
 
 
 def extraer_texto(
     ruta_archivo: str
 ):
 
-    resultado = reader.readtext(
+    resultado = get_reader().readtext(
         ruta_archivo,
         detail=0
     )
