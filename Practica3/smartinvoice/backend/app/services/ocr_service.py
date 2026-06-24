fb2def0982119abc47e5f49ae641362433931c4d
Alejandro Marin <3019371840101@ingenieria.usac.edu.gt>
2026-06-23 23:57:02 -0600

    deploy

--- Practica3/smartinvoice/backend/app/services/ocr_service.py
+++ Practica3/smartinvoice/backend/app/services/ocr_service.py
@@ -1,15 +1,22 @@
 import easyocr
 
+reader = None
 
-reader = easyocr.Reader(
-    ['es'],
-    gpu=False
-)
+def get_reader():
+    global reader
+
+    if reader is None:
+        reader = easyocr.Reader(
+            ['es'],
+            gpu=False
+        )
+
+    return reader
 
 
-def extraer_texto(
-    ruta_archivo: str
-):
+def extraer_texto(ruta_archivo):
+
+    reader = get_reader()
 
     resultado = reader.readtext(
         ruta_archivo,
