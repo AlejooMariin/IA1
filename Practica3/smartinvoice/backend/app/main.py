2317ad41b89dd9664ee2b787b2164ae072f73e3a
Alejandro Marin <3019371840101@ingenieria.usac.edu.gt>
2026-06-24 01:07:52 -0600

    deploy

--- Practica3/smartinvoice/backend/app/main.py
+++ Practica3/smartinvoice/backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://tu-proyecto.vercel.app"
+    "https://ia-1-git-feature-practica3-deploy-alejo97.vercel.app"
 ]
 
 app.add_middleware(
